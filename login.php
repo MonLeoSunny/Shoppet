e0f4d1f021d7c10a6e23db7c8f2ddad4ee0ad3f9
MonLeoSunny <112103842+MonLeoSunny@users.noreply.github.com>
2023-11-22 23:23:28 +0700

    fix

--- login.php
+++ login.php
@@ -136,29 +136,48 @@
     <!--================login_part end =================-->
 
     <!--::footer_part start::-->
-    <footer class="footer_part">
+    <footer class="footer_part" style=" margin-top: 10px; padding: 20px; background-color: #F8F8F8; background-clip: padding;">
         <div class="container">
             <div class="row justify-content-around">
-                <div class="col-sm-6 col-lg-2">
+                <div class="col-sm-6 col-lg-3">
                     <div class="single_footer_part">
-                        <h4>Features</h4>
+                        <h4>Thông Tin Cửa Hàng</h4>
                         <ul class="list-unstyled">
-                            <li><a href="">Jobs</a></li>
-                            <li><a href="">Brand Assets</a></li>
-                            <li><a href="">Investor Relations</a></li>
-                            <li><a href="">Terms of Service</a></li>
+                            <li><a href="">Cửa Hàng </a></li>
+                            <li><a href="">Số 34 Trưng Nhị, Hà Đông, Tp. Hà Nội</a></li>
+                            <li><a href="">Hotline: 024 6259 3434</a></li>
                         </ul>
                     </div>
                 </div>
-                <div class="col-sm-6 col-lg-2">
+                
+                <div class="col-sm-6 col-lg-3">
                     <div class="single_footer_part">
-                        <h4>Resources</h4>
+                        <h4>Chính Sách</h4>
                         <ul class="list-unstyled">
-                            <li><a href="">Guides</a></li>
-                            <li><a href="">Research</a></li>
-                            <li><a href="">Experts</a></li>
-                            <li><a href="">Agencies</a></li>
+                            <li><a href="">Quy chế hoạt động</a></li>
+                            <li><a href="">Chính sách Bảo hành</a></li>
+                            <li><a href="">Liên hệ hợp tác kinh doanh</a></li>
+                            <li><a href="">Đơn Doanh nghiệp</a></li>
                         </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="copyright_part">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8">
+                        <!--  -->
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="footer_icon social_icon">
+                            <ul class="list-unstyled">
+                                <li><a href="" class="single_social_icon"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="" class="single_social_icon"><i class="fab fa-instagram"></i></a></li>
+                                <li><a href="" class="single_social_icon"><i class="fas fa-globe"></i></a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
